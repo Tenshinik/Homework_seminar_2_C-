0048f6f4ecf084cde1da6ba3ad7bb90c374f9a30
Tenshinik <@tenshinik@gmail.com>
2022-12-05 23:01:00 +0800

    Seminar002 zadacha001 project

--- Homework002/zadacha001/Program.cs
+++ Homework002/zadacha001/Program.cs
@@ -6,3 +6,21 @@
 
 */
 
+Console.WriteLine("Enter a three-digit number");
+
+string? ThreeDigitNumber = Console.ReadLine();
+
+if (ThreeDigitNumber == null || ThreeDigitNumber.Length != 3)   
+{
+    Console.WriteLine("this is an incorrect number");
+    Environment.Exit(1);
+}
+
+Console.WriteLine(ThreeDigitNumber?[1]);
+
+
+
+
+
+
+
